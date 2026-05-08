93c857d8043cdba24917bf9426a69724c5c2ecbf
Mack Mirosh <mack.mirosh@outlook.com>
2026-05-08 15:15:19 -0400

    Add project files.

---
+++ exceptions/Source.cpp
@@ -0,0 +1,24 @@
+// Mack Miroshnychenko
+// CIS 1202 800
+// 5/8/26
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+class invalidCharacterException {};
+class invalidRangeException {};
+
+char character(char start, int offset);
+
+int main() {
+    char start;
+    int offset;
+    
+    return 0;
+}
+
+char character(char start, int offset) {
+    
+}
